e6ea3d5ff986d6b7ce2d3d7fb382ad9e8fee5ab4
Thibaut Horel <thibaut.horel@gmail.com>
2014-07-09 08:54:50 -0400

    [Firefox add-on] also track active/inactive window

--- firefox-addon/lib/main.js
+++ firefox-addon/lib/main.js
@@ -1,8 +1,12 @@
+var windows = require("sdk/windows").browserWindows;
 var tabs = require("sdk/tabs");
 var Request = require("sdk/request").Request;
 var prefs = require("sdk/simple-prefs").prefs;
 
 function logUrl(tab) {
+    if (tab !== tabs.activeTab) {
+        return;
+    }
     Request({
         url: prefs.callbackUrl,
         headers: {
@@ -13,5 +17,18 @@ function logUrl(tab) {
     }).post();
 };
 
+function logDesactivate() {
+    Request({
+        url: prefs.callbackUrl,
+        headers: {
+            "url": null,
+            "time": Date.now(),
+            "title": null,
+        }
+    }).post();
+}
+
 tabs.on("activate", function () { logUrl(tabs.activeTab) });
 tabs.on("pageshow", function(tab) { logUrl(tab) });
+windows.on("activate", function () { logUrl(tabs.activeTab) });
+windows.on("deactivate", function () { logDesactivate() });
